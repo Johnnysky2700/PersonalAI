892e851c70e580495a670cba103bd9df5cb299e5
Johnnysky <vanpasie20@gmail.com>
2026-02-23 09:38:43 -0800

    completing the hero and navbar components with user authentication implemented

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Hero from '../Components/Hero'
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 }
 
