68f743f23801de07d9e878378cdcc2a3ac21ebe8
Johnnysky <vanpasie20@gmail.com>
2026-02-27 06:29:15 -0800

    working on sidebar and layout

--- client/src/Pages/Layout.jsx
+++ client/src/Pages/Layout.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { Menu, X } from 'lucide-react'
+import Sidebar from '../Components/Sidebar'
+import { SignIn, useUser } from '@clerk/clerk-react'
 
 const Layout = () => {
-  return (
-    <div>
-        <h1>Layout</h1>
-        <Outlet />
+
+  const navigate = useNavigate()
+  const [sidebar, setSidebar] = useState(false)
+  const {user} = useUser()
+
+  return user ? (
+    <div className='flex flex-col h-screen'>
+      <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'>
+        <img src={assets.logo} alt="" onClick={() => navigate('/')} className='h-12' />
+        {
+          sidebar ? <X onClick={() => setSidebar(false)} className="w-6 h-6 text-gray-600 sm:hidden" />
+            : <Menu onClick={() => setSidebar(true)} className="w-6 h-6 text-gray-600 sm:hidden" />
+        }
+      </nav>
+      <div className='flex flex-1 w-full h-[calc(100vh-56px)]'>
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className='flex-1 bg-[#f4f7fb] overflow-auto'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className='flex items-center justify-center h-screen'>
+       <SignIn />
     </div>
   )
 }
